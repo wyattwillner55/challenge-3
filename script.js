c1c42945401d81c7a8c5ce16ca36262547e2e490
wyattwillner55 <109093691+wyattwillner55@users.noreply.github.com>
2022-07-21 17:17:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function resetCon(){
   symConfirm = false;
 }
 
- //  Checks that all parts required are in it.
+//  Checks that all parts required are in it.
 function checkPassword() {
   for (var x = 0; x < password.length; x++){
     for (var a = 0; a < caps.length; a++){
@@ -97,49 +97,49 @@ function lengthCheck() {
 }
 
 function lengthPrompt() {
-  length = prompt("Choose your password size between 8 and 128 characters.");
-  if (false == lengthCheck()) {
+  length = prompt("Pick a number between 8 and 128 for your password size.");
+  if (lengthCheck() == false) {
     alert("Please pick a valid answer");
-    lengthPrompt();
+  }
+  else {
+    return true;
   }
 }
 
 // Write password to the #password input
 function writePassword() {
-  lengthPrompt();
+  if (lengthPrompt() == true) {
     if (confirm("Do you want lowercase letters in your password?") == true)  {
-      charset += "abcdefghijklmnopqrstuvwxyz";
-      lowNeeded = true;
+        charset += "abcdefghijklmnopqrstuvwxyz";
+        lowNeeded = true;
     } else {
-      lowNeeded = false;
+        lowNeeded = false;
     }
-    console.log(charset);
     if (confirm("Do you want capital letters in your password?") == true)  {
-      charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      capNeeded = true;
+        charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        capNeeded = true;
     } else {
-      capNeeded = false;
+        capNeeded = false;
     }
-    console.log(charset);
     if (confirm("Do you want numbers in your password?") == true)  {
-      charset += "0123456789";
-      numNeeded = true;
+        charset += "0123456789";
+        numNeeded = true;
     } else {
-      numNeeded = false;
+        numNeeded = false;
     }
-    console.log(charset);
     if (confirm("Do you want symbols in your password?") == true)  {
-      charset += "!@#$%^&*?";
-      symNeeded = true;
+        charset += "!@#$%^&*?";
+        symNeeded = true;
     } else  { 
-      symNeeded = false;
+        symNeeded = false;
     }
-    console.log(charset);
     var password = generatePassword();
     var passwordText = document.querySelector('#password');
 
     passwordText.value = password;
- charset = "";
+  }
+    charset = "";
+
 }
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
